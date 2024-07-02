53190060c6392b6c5d6a16a592f15796b9c697ef
vicky23sri <vickysrini28@gmail.com>
2024-07-02 15:07:53 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          50: '#e6fffa',
+          100: '#b2f5ea',
+          200: '#81e6d9',
+          300: '#4fd1c5',
+          400: '#38b2ac',
+          500: '#319795',
+          600: '#2c7a7b',
+          700: '#285e61',
+          800: '#234e52',
+          900: '#1d4044',
+          primary: '#18A7B8', 
+        },
+      },
+    },
+  },
+  plugins: [],
+};
